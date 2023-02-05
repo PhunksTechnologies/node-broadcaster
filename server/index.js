803b6ead3007fa9bb994b9e12ad724d9770ffb29
PhunksTechnologies <justadmin@rambler.ru>
2023-02-06 02:22:59 +0400

    still trying to implement whois lookup4

--- server/index.js
+++ server/index.js
@@ -136,10 +136,10 @@ async function getIPInfo(ip){
 
     axios(config)
     .then(function (response) {
-        console.log(response);
-        let country = response.body.list[0].html.substring(response.body.list[0].html.indexOf('country')+17, response.body.list[0].html.indexOf('\\'));
-        let city = response.body.list[0].html.substring(response.body.list[0].html.indexOf('city')+14, response.body.list[0].html.indexOf('\\'));
-        let descr = response.body.list[0].html.substring(response.body.list[0].html.indexOf('descr')+15, response.body.list[0].html.indexOf('\\'));
+        // console.log(response);
+        let country = response.data.body.list[0].html.substring(response.data.body.list[0].html.indexOf('country')+17, response.data.body.list[0].html.indexOf('\\'));
+        let city = response.data.body.list[0].html.substring(response.data.body.list[0].html.indexOf('city')+14, response.data.body.list[0].html.indexOf('\\'));
+        let descr = response.data.body.list[0].html.substring(response.data.body.list[0].html.indexOf('descr')+15, response.data.body.list[0].html.indexOf('\\'));
         return country + ', '+ city + ', ' + descr;
     })
     .catch(function (error) {
